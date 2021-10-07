b0192dcfd9f21b65e7e52ef72de1628d08ceccee
Saiful Islam <saifhd85@gmail.com>
2021-10-07 17:33:26 +0530

    disabled user registration

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
+Route::any('/register', function () {
+    abort(403);
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
